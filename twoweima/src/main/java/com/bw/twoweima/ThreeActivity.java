e89ca1e168674f0f5ed6b2754cc81e730e52d2a2
yanghzong <zemu.zhong@qq.com>
2018-10-09 09:04:23 +0800

    哒哒哒对对对

--- twoweima/src/main/java/com/bw/twoweima/ThreeActivity.java
+++ twoweima/src/main/java/com/bw/twoweima/ThreeActivity.java
@@ -3,11 +3,11 @@ package com.bw.twoweima;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Th extends AppCompatActivity {
+public class ThreeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_th);
+        setContentView(R.layout.activity_three);
     }
 }
